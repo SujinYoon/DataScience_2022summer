6e16142ec6eae4ea4f1abbd5fe4d849ab6d9c85c
SujinYoon <sujin.yoon98@gmail.com>
2022-08-01 13:56:56 +0900

    Initial commit

---
+++ tf.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import numpy as np
+import time
+import streamlit as st
+st.title("Keras Training Basic UI")
+st.header("A Streamlit based Web UI To Create And Train Models")
+st.subheader("Create Network:")
